74c775a78c29979662d374dd78b7922c47a98ca9
Raimundas Banevicius <rbanevicius@alnasoft.lt>
2020-03-16 21:00:50 +0200

    find max number function added.

--- SampleGithubProject/SampleGithubProject/Program.cs
+++ SampleGithubProject/SampleGithubProject/Program.cs
@@ -6,6 +6,21 @@ namespace SampleGithubProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(FindBiggestNumber(10, 20, 30));
         }
+
+        static int FindBiggestNumber(int number1, int number2, int number3)
+        {
+            if (number1 >= number2 && number1 >= number3)
+            {
+                return number1;
+            }
+            if (number2 >= number3)
+            {
+                return number2;
+            }
+            return number3;
+        }
+
     }
 }
