047ec560eee71abbb0f66ed6979c9e813576488b
Raimundas Banevicius <rbanevicius@alnasoft.lt>
2020-03-16 20:38:56 +0200

    initial commit.

---
+++ SampleGithubProject/SampleGithubProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleGithubProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
