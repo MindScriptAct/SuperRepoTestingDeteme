4154c3b8e639d340c95ae96927d2b357f1bc01a6
Raimundas Banevicius <rbanevicius@alnasoft.lt>
2020-03-16 21:23:00 +0200

    test if number primal added.

--- SampleGithubProject/SampleGithubProject/Program.cs
+++ SampleGithubProject/SampleGithubProject/Program.cs
@@ -6,7 +6,20 @@ namespace SampleGithubProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsPrimal(13));
         }
+
+        static bool IsPrimal(int number)
+        {
+            for (int i = 2; i < number / i; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 }
